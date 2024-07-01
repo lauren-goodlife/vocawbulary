c6f4f63003c50d6858430257641fa6e90763c96a
Lorena <106349039+lauren-goodlife@users.noreply.github.com>
2024-07-01 10:28:34 +0200

    Add responsive

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+
+
+
+
 /* document.addEventListener('DOMContentLoaded', function() {
     const searchButton = document.getElementById('searchButton');
     const searchInput = document.getElementById('searchInput');
